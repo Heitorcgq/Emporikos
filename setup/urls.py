e669f51d48f04e1ce365d9f689ae09e85f2422a7
Heitor Queiroz <heitorqueirozes@gmail.com>
2025-12-04 17:15:49 -0300

    pagina de login e botao de logout

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from apps.loja import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
     path('', views.frente_caixa, name='frente_caixa'),
     path('produtos/novo/', views.cadastro_produto, name='cadastro_produto'),
