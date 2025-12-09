16490a8409695a44566984fd1106027e15ba0d11
Heitor Queiroz <heitorqueirozes@gmail.com>
2025-12-09 15:51:43 -0300

    Sistema de troco

--- setup/settings.py
+++ setup/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Configuração para PostgreSQL lendo do .env
+# Configuração para PostgreSQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -80,6 +80,13 @@ DATABASES = {
     }
 }
 
+# --- SQLite - Muito mais leve ---
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
